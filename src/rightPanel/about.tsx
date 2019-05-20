bdcc224ef1a7b3cecd471071a39a3369f8f03764
Matthias Müller-Brockhausen <git@mmb2.click>
2019-05-20 18:11:30 +0200

    :sparkles:

--- src/rightPanel/about.tsx
+++ src/rightPanel/about.tsx
@@ -1,9 +1,10 @@
-import { AppBar, Button, Dialog, DialogContent, IconButton, Slide, Toolbar } from "@material-ui/core"
+import { AppBar, Button, Dialog, DialogContent, IconButton, Link, Slide, Toolbar } from "@material-ui/core"
 import ExpansionPanel from "@material-ui/core/ExpansionPanel"
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails"
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary"
 import Typography from "@material-ui/core/Typography"
 import CloseIcon from "@material-ui/icons/Close"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import { observable } from "mobx"
 import { observer } from "mobx-react-lite"
 import * as React from "react"
@@ -30,25 +31,25 @@ const handleOpen = () => isOpen.set(true)
 const AboutPanel = observer(() => {
   return (
     <ExpansionPanel defaultExpanded>
-      <ExpansionPanelSummary>
+      <ExpansionPanelSummary  expandIcon={<ExpandMoreIcon />}>
         <Typography>About</Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
         <Typography>
-          Written with ❤️ by <a>hizoul</a><br/>
+          Written with ❤️ by <Link href="https://github.com/hizoul">hizoul</Link>.<br/>
           Special thanks to&nbsp;
-          <a href="https://github.com/JuliusSweetland">Julius Sweetland</a>
+          <Link href="https://github.com/JuliusSweetland">Julius Sweetland</Link>
           &nbsp;for writing&nbsp;
-          <a href="https://github.com/OptiKey/OptiKey">
+          <Link href="https://github.com/OptiKey/OptiKey">
             OptiKey
-          </a> and&nbsp;
-          <a
+          </Link> and&nbsp;
+          <Link
             onClick={handleOpen}
             href="#"
           >
-            all other authors of open source libraries
-          </a>
-          &nbsp;that make this small tool possible
+            all authors of open source libraries
+          </Link>
+          &nbsp;enabling the existence of this tool.
         </Typography>
       </ExpansionPanelDetails>
       <Dialog
